7729a459b65a3b4d8866622b6dc32518c1ccb302
Mohamed Magdy Gamal <64127744+mohamedmagdy841@users.noreply.github.com>
2025-12-12 13:09:25 +0200

    prisma setup

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,6 @@
 
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '../generated/prisma/client';
+import { PrismaClient } from '../../generated/prisma/client';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
 @Injectable()
